35488ba8367c4cfb15bd05b538a4df4f98f2718b
dbv-data-db <damiebrooks@virtusa.com>
2024-08-01 11:54:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ with st.sidebar:
     selected_category = st.selectbox('Select a Category', cat_list)
     df_selected_category = df_reshaped[df_reshaped.category == selected_category]
     df_selected_category_sorted = df_selected_category.sort_values(by="priority", ascending=False)
-
+    st.markdown('list out the subs here.')
+    st.markdown(list(df_reshaped.subcategory.unique())[::-1])
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 #######################

6b6e93e50d97a099ebad0bf7537b4a4379834f5e
dbv-data-db <damiebrooks@virtusa.com>
2024-08-01 13:18:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,23 @@ with st.sidebar:
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 #######################
 # Plots
-
+# Heatmap
+def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
+    heatmap = alt.Chart(input_df).mark_rect().encode(
+            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Priority", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+            color=alt.Color(f'max({input_color}):Q',
+                             legend=None,
+                             scale=alt.Scale(scheme=input_color_theme)),
+            stroke=alt.value('black'),
+            strokeWidth=alt.value(0.25),
+        ).properties(width=900
+        ).configure_axis(
+        labelFontSize=12,
+        titleFontSize=12
+        ) 
+    # height=300
+    return heatmap
 
 #######################
 # Dashboard Main Panel
@@ -49,7 +65,8 @@ with col[0]:
 
 with col[1]:
     st.markdown('#### Total Population')
-
+    heatmap = make_heatmap(df_reshaped, 'priority', 'subcategory', 'adobescore', selected_color_theme)
+    st.altair_chart(heatmap, use_container_width=True)
     
 
 with col[2]:

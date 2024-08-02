3a08cc2367b990085435e75e520c1a1a2ffd21be
dbv-data-db <damiebrooks@virtusa.com>
2024-08-02 11:01:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,6 +167,7 @@ with col[1]:
     st.markdown('#### Feature Scoring')
     heatmap = make_heatmap(df_reshaped, 'subCategory', 'adobeScore', 'priority', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
+    sf.dataframe(df_selected_category)
 #df_selected_category is used for filtering
 
     

8a4bc0e9f4ef7414e335ad809a1e556f808e3233
dbv-data-db <damiebrooks@virtusa.com>
2024-08-06 16:54:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -173,4 +173,12 @@ with col[1]:
     st.altair_chart(heatmap, use_container_width=True)  
     fig = px.imshow(hots, text_auto=True)
     st.plotly_chart(fig, use_container_width=True)
-    
+    data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
+    fig2 = px.imshow(data,
+                labels=dict(x="Day of Week", y="Time of Day", color="Productivity"),
+                x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                y=['Morning', 'Afternoon', 'Evening']
+               )
+    fig2.update_xaxes(side="top")
+    st.plotly_chart(fig2, use_container_width=True)
+

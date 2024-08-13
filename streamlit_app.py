78b5707f1583a091ea7d80923e86d4403d18851b
dbv-data-db <damiebrooks@virtusa.com>
2024-08-13 13:27:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,18 +157,7 @@ with col[1]:
     fig = px.pie(weightdata, values='weight', names='category', title=f'Feature Priotization', height=500, width=300)
     st.plotly_chart(fig, use_container_width=True)    
     st.markdown('#### (Weighted) Capability Scoring')
-    #hardcoding in the summary numbers for now
-    #adobe: 4.08, 3.91, 4.25, 2.38, 8.33, 3.23
-    #sf: 4.08, 3.91, 4.23, 2.38, 1.7, 2.04
-    #braze: 2.55, 3.57, 3.4, 1.53, 3.74, 2.21 
-    data=[[4.32, 4.6, 4.05, 1,4, 12.25, 2.28], [4.32, 4.6, 3.45, 1.4, 7.25, 1.8]]
-    fig2 = px.imshow(data,
-                labels=dict(x="Vendor", y="Category", color="Score"),
-                x=['Data', 'Decisioning', 'Distribution', 'Measurement', 'Content','Experience'],
-                y=['Adobe', 'Salesforce']
-               )
-    fig2.update_xaxes(side="top")
-    st.plotly_chart(fig2, use_container_width=True)
+
     st.markdown('### (Unweighted) Dimensional Scoring')
     #hardcoding in the summary numbers for now
     data=[[4, 4, 4, 5, 5, 4, 5, 5], [3, 4, 4, 4, 5, 4, 5, 3]]
